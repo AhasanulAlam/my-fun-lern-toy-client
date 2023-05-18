a117faefebf283c61af6a34d3ca160bdb63f6346
AhasanulAlam <ahasanulalam@gmail.com>
2023-05-19 01:18:58 +0800

    update the firebase config file and env variable

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,12 @@
 import Lottie from "lottie-react";
 import loginPic from '../../assets/loginPic.json';
+import { Link } from "react-router-dom";
 
 const Login = () => {
     const handleLogin = (event) =>{
         event.preventDefault();
         const form = event.target;
-        
+
 
     }
 
@@ -25,13 +26,13 @@ const Login = () => {
                                     <label className="label">
                                         <span className="label-text">Email</span>
                                     </label>
-                                    <input type="text" placeholder="email" className="input input-bordered" />
+                                    <input type="email" name="email" placeholder="email" className="input input-bordered" />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="text" placeholder="password" className="input input-bordered" />
+                                    <input type="password" name="password" placeholder="password" className="input input-bordered" />
                                     <label className="label">
                                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                     </label>
@@ -40,6 +41,7 @@ const Login = () => {
                                     <input className="btn btn-primary" type="submit" value="Login" />
                                 </div>
                             </form>
+                            <p className="my-4 tex-center">New to FunLearnToy? <Link className="text-orange-600 font-semibold" to="/signup" >Sign Up</Link> </p>
                         </div>
                     </div>
                 </div>
