885b9b8e5c289c6e5c7d286a680947959b3c87f4
AhasanulAlam <ahasanulalam@gmail.com>
2023-05-19 00:35:44 +0800

    Added Category data in the Tab by category

--- src/pages/ShopByCategory/ShopByCategory.jsx
+++ src/pages/ShopByCategory/ShopByCategory.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const ShopByCategory = () => {
     const [tabIndex, setTabIndex] = useState(0);
+
+    const [catToys, setCatToys] = useState([]);
+
+    const resultScienceToy = catToys.filter(toy => toy.sub_category === "Science Toys" )
+    const resultMathToy = catToys.filter(toy => toy.sub_category === "Math Toys" )
+    const resultLanguageToy = catToys.filter(toy => toy.sub_category === "Language Toys" )
+
+    useEffect(() => {
+        fetch('toysData.json')
+            .then(res => res.json())
+            .then(data => setCatToys(data))
+    }, []);
 
     return (
         <div className='mt-16 mb-32'>
@@ -17,12 +29,21 @@ const ShopByCategory = () => {
 
                 <TabPanel>
                     <h2>Any content of Science Toys</h2>
+                    {
+                        resultScienceToy.map(toy => <p key={toy._id}>{toy.name}</p>)
+                    }
                 </TabPanel>
                 <TabPanel>
                     <h2>Any content of Math Toys</h2>
+                    {
+                        resultMathToy.map(toy => <p key={toy._id}>{toy.name}</p>)
+                    }
                 </TabPanel>
                 <TabPanel>
                     <h2>Any content of Language Toys</h2>
+                    {
+                        resultLanguageToy.map(toy => <p key={toy._id}>{toy.name}</p>)
+                    }
                 </TabPanel>
             </Tabs>
         </div>
