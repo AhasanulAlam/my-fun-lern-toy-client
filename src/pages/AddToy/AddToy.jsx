67439ad2d5a3b2dd0818e6e7de85254e7520a2cd
AhasanulAlam <ahasanulalam@gmail.com>
2023-05-19 19:23:16 +0800

    created object of add new toy

--- src/pages/AddToy/AddToy.jsx
+++ src/pages/AddToy/AddToy.jsx
@@ -7,19 +7,33 @@ const AddToy = () => {
     const handleAddToy = (event) => {
         event.preventDefault();
         const form = event.target;
-        const pictureUrl = form.pictureUrl.value;
+        const picture_url = form.picture_url.value;
         const name = form.name.value;
-        const sellerName = form.sellerName.value;
-        const sellerEmail = form.sellerEmail.value;
-        const category = form.category.value;
+        const seller_name = form.seller_name.value;
+        const seller_email = form.seller_email.value;
+        const sub_category = form.sub_category.value;
         const price = form.price.value;
         const rating = form.rating.value;
-        const availableQuantity = form.availableQuantity.value;
-        const createdBy = user.email;
+        const available_quantity = form.available_quantity.value;
+        const created_by = user.email;
         const description = form.description.value;
 
+        const newToy = {
+            picture_url,
+            name,
+            seller_name,
+            seller_email,
+            sub_category,
+            price,
+            rating,
+            available_quantity,
+            created_by,
+            description
+        }
 
-        console.log(pictureUrl, name, sellerName, sellerEmail, category,price, rating, availableQuantity, createdBy, description );
+
+        console.log(picture_url, name, seller_name, seller_email, sub_category,price, rating, available_quantity, created_by, description );
+        console.log(newToy);
 
     }
 
@@ -41,13 +55,13 @@ const AddToy = () => {
                                         <label className="label">
                                             <span className="label-text">Seller Name</span>
                                         </label>
-                                        <input type="text" name="sellerName" placeholder="name" className="input input-bordered" required />
+                                        <input type="text" name="seller_name" placeholder="name" className="input input-bordered" required />
                                     </div>
                                     <div className="form-control w-full">
                                         <label className="label">
                                             <span className="label-text">Seller Email</span>
                                         </label>
-                                        <input type="email" name="sellerEmail" placeholder="email" className="input input-bordered" required />
+                                        <input type="email" name="seller_email" placeholder="email" className="input input-bordered" required />
                                     </div>
                                 </div>
                                 <div className='grid grid-cols-2 gap-4'>
@@ -63,7 +77,7 @@ const AddToy = () => {
                                             <label className="label">
                                                 <span className="label-text">Toy Category</span>
                                             </label>
-                                            <select name='category' className="input input-bordered">
+                                            <select name='sub_category' className="input input-bordered">
                                                 <option>Select Toy Category</option>
                                                 <option value="Math Toys">Math Toys</option>
                                                 <option value="Science Toys">Science Toys</option>
@@ -74,14 +88,14 @@ const AddToy = () => {
                                             <label className="label">
                                                 <span className="label-text">Toy price</span>
                                             </label>
-                                            <input type="number" name='price' placeholder="price" className="input input-bordered" required />
+                                            <input type="text" name='price' placeholder="price" className="input input-bordered" required />
                                         </div>
                                     </div>
 
                                     <div className='flex flex-col'>
                                         <div className="form-control">
                                             <label className="label">
-                                                <span className="label-text">Toy Rating</span>
+                                                <span className="label-text">Toy Rating (1 to 5)</span>
                                             </label>
                                             <input type="number" name='rating' placeholder="rating" className="input input-bordered" required />
                                         </div>
@@ -89,13 +103,13 @@ const AddToy = () => {
                                             <label className="label">
                                                 <span className="label-text">Available Quantity</span>
                                             </label>
-                                            <input type="number" name='availableQuantity' placeholder="rating" className="input input-bordered" required />
+                                            <input type="number" name='available_quantity' placeholder="quantity" className="input input-bordered" required />
                                         </div>
                                         <div className="form-control">
                                             <label className="label">
                                                 <span className="label-text">Toy Picture</span>
                                             </label>
-                                            <input type="text" name='pictureUrl' placeholder="photo URL" className="input input-bordered" required />
+                                            <input type="text" name='picture_url' placeholder="photo URL" className="input input-bordered" required />
                                         </div>
                                     </div>
                                 </div>
@@ -107,7 +121,7 @@ const AddToy = () => {
                                     </div>
 
                                 <div className="form-control">
-                                    <input type="email" name='createdBy' defaultValue={user?.email} placeholder="email_User " className="input input-bordered" hidden />
+                                    <input type="email" name='created_by' defaultValue={user?.email} placeholder="email_User " className="input input-bordered" hidden />
                                 </div>
                                 <div className="form-control mt-6">
                                     <input className="btn btn-primary" type="submit" value="Add" />
