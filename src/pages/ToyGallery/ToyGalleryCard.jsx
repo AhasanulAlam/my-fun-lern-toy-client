e79d561c2755d7503290846f1e7a0b42cf6bb308
AhasanulAlam <ahasanulalam@gmail.com>
2023-05-20 01:16:38 +0800

    add AOS in the Gallery pic

--- src/pages/ToyGallery/ToyGalleryCard.jsx
+++ src/pages/ToyGallery/ToyGalleryCard.jsx
@@ -1,8 +1,11 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
 
 const ToyGalleryCard = ({toys}) => {
     const {picture_url} = toys;
     return (
-        <div>
+        <div data-aos="zoom-in" >
             <div className="card card-compact w-96 bg-base-900 shadow-xl">
                 <div className="card-body">
                 <figure className="rounded-2xl"><img className="h-80" src={picture_url} alt="toy Picture"/></figure>
